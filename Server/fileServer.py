04b4bff3293f5384486c29405c3ddb264fe862ed
George Doxastakis <ee06513@teiath.gr>
2016-11-19 17:31:23 +0200

    [Server] Set port

--- Server/fileServer.py
+++ Server/fileServer.py
@@ -4,10 +4,11 @@ app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = '/home/gdoxastakis/ServerStorage'
 @app.route('/upload', methods = ['GET', 'POST'])
 def upload_file():
-   if request.method == 'POST':
-      f = request.files['file']
-      f.save(secure_filename(f.filename))
-      return 'OK'
-		
+	if request.method == 'POST':
+		f = request.files['file']
+		f.save(secure_filename(f.filename))
+		return 'OK'
+	else:
+		return 'Server Online'
 if __name__ == '__main__':
-   app.run(debug = True)
+   app.run(app.run(host='83.212.116.82', port=9000, debug=True))
