d9d54501544c611552f5c9fb34144890e9acfbd0
George Doxastakis <ee06513@teiath.gr>
2016-11-19 17:01:00 +0200

    [Server] Set storage path

--- Server/fileServer.py
+++ Server/fileServer.py
@@ -1,13 +1,13 @@
 from flask import Flask, render_template, request
 from werkzeug import secure_filename
 app = Flask(__name__)
-	
+app.config['UPLOAD_FOLDER'] = '/home/gdoxastakis/ServerStorage'
 @app.route('/upload', methods = ['GET', 'POST'])
 def upload_file():
    if request.method == 'POST':
       f = request.files['file']
       f.save(secure_filename(f.filename))
-      return 'file uploaded successfully'
+      return 'OK'
 		
 if __name__ == '__main__':
    app.run(debug = True)
