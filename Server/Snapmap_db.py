ec7c54dc557d4a60b28388e68ef24ba444e038c4
DDounis <ee06514@teiath.gr>
2016-11-26 19:40:37 +0200

    Updated db file

--- Server/Snapmap_db.py
+++ Server/Snapmap_db.py
@@ -1,65 +1,58 @@
 import sqlite3
-import random
 
 
-conn = sqlite3.connect('Snapmap.db')
-c = conn.cursor()
+def open_db():
+    conn = sqlite3.connect('Snapmap.db')
+    c = conn.cursor()
+    return c, conn
 
 
 def create_table():
-    c.execute('CREATE TABLE IF NOT EXISTS images(Number INTEGER, Image_Name TEXT, Longitude TEXT, Latitude TEXT)')
-
-'#insert variables here'
-
-
-def data_entry():
-    c.execute("INSERT INTO images VALUES(1, 'image.jpeg', 'Longitude_1', 'Latitude_1')")
-    conn.commit()
-
-create_table()
-data_entry()
+    conn = sqlite3.connect('Snapmap.db')
+    c = conn.cursor()
+    c.execute('CREATE TABLE IF NOT EXISTS \
+    Images(Name TEXT, Lat FLOAT, Long FLOAT)')
 
 
-def dynamic_data_entry():
-    new_number = random.randrange(0,10)
-    c.execute('INSERT INTO images (Number) VALUES(?)', new_number)
+def add_entry(name, lon, lat):
+    conn = sqlite3.connect('Snapmap.db')
+    c = conn.cursor()
+    lat = float(lat)
+    lon = float(lon)
+    c.execute('INSERT INTO images (Name, Lat, Long)\
+    VALUES (?, ?, ?)', (name, lat, lon))
     conn.commit()
 
 
-def read_from_db():
-    c.execute('SELECT * FROM images')
-    # c.execute('SELECT Image_Name FROM images WHERE Longitude<Distance AND Latitude<Distance')
-    [print(row) for row in c.fetchall()]
+def read_from_db(user_lat, user_lon):
+    conn = sqlite3.connect('Snapmap.db')
+    c = conn.cursor()
+    lat = float(user_lat)
+    lon = float(user_lon)
+    maxlat = lat+0.000896
+    minlat = lat-0.000896
+    maxlon = lon+0.001140
+    minlon = lon-0.001140
+    c.execute('SELECT Name FROM images \
+    WHERE Lat<? AND Lat>? AND Long<? AND Long>?', (maxlat, minlat, maxlon, minlon))
+    for row in c.fetchall():
+        return row
 
-read_from_db()
 
-
-def update():
-    c.execute("UPDATE images SET Image_Name = 'newimage.jpeg' WHERE Image_Name='image.jpeg'")
+def delete(name):
+    conn = sqlite3.connect('Snapmap.db')
+    c = conn.cursor()
+    c.execute('DELETE FROM images WHERE Name = ?', name)
     conn.commit()
 
-    c.execute('SELECT * FROM images')
-    [print(row) for row in c.fetchall()]
 
-update()
-
-
-def delete():
-    c.execute('DELETE FROM images WHERE Number = 5')
+def clear():
+    conn = sqlite3.connect('Snapmap.db')
+    c = conn.cursor()
+    c.execute('DELETE * FROM images')
     conn.commit()
-    print(50*'#')
-    c.execute('SELECT * FROM images')
-    [print(row) for row in c.fetchall()]
-
-delete()
 
 
-def counter():
-    c.execute('SELECT * FROM images WHERE Number = 1')
-    print(len(c.fetchall()))
-
-counter()
-
-
-# c.close()
-# conn.close()
+def close_db(c, conn):
+    c.close()
+    conn.close()
